f5f04a27b3fdc6e932d3b057a87cd38fafd03d93
BG-user-OK <GitHub@bogdangawel.pl>
2026-05-07 21:30:57 +0200

    Add files via upload

--- app.js
+++ app.js
@@ -5,7 +5,7 @@
 // ---------------------------------------------------------------
 // 0. WERSJA APLIKACJI
 // ---------------------------------------------------------------
-const APP_VERSION = '0.6.9';
+const APP_VERSION = '0.7.0';
 
 // Lista rzeczy do spakowania
 const PACK_ITEMS = [
